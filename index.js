cde56c52656ac974a09cf75b224ac34ba86ad8d4
Itallohsa <itallohsa@hotmail.com>
2022-10-24 17:59:10 -0300

    adicionando laterais da ampulheta

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let Hourglass = (size, inc, dec) => {
+const Hourglass = (size, inc, dec) => {
     let interval = setInterval(function () {
         console.clear()
         let x = 0
@@ -7,17 +7,44 @@ let Hourglass = (size, inc, dec) => {
         let i, j;
 
         for (i = 1; i <= size; i++) {
+
             for ( j = 1; j <= x; j++) {
                 string += " "
+                if(j == 1) {
+                    string += "*"
+                };
             };
             for (j = 1; j <= y; j++) {
-                if (i <= dec + 1 && i > size / 2 && i < size && j > 1 && j < y) {
-                    string += " "
-                } else if (i < inc && i < size / 2 + 1 && i > 1 && j > 1 && j < y) {
-                    string += " "
-                } else {
+                if(i == 2) {
                     string += "*"
+                };
+
+                if(size % 2 == 0) {
+                    if(i == size) {
+                        string += "*"
+                    };
+
+                } else {
+                    if(i == size - 1) {
+                        string += "*"
+                    };
+                };
+
+                if (i <= dec + 1 && i > size / 2 && i < size && i != size - 1 && j > 1 && j < y) {
+                    string += " "
+                } else if (i < inc && i < size / 2 + 1 && i != 2 && i > 1 && j > 1 && j < y) {
+                    string += " "
+                } else if(i != 1 && i != 2 && i != size && i != size - 1){
+                    string += "*"
+                };
+            };
+            for ( j = 1; j <= x; j++) {
+                if(j > 1) {
+                    string += " "
                 }
+                if(j == x) {
+                    string += "*"
+                };
             };
             if (i <= size / 2) {
                 y -= 2;
@@ -25,10 +52,11 @@ let Hourglass = (size, inc, dec) => {
             } else {
                 y += 2;
                 x--;
-            }
+            };
             console.log(string)
             string = ''
         };
+
         inc++
         dec--
 
@@ -39,7 +67,7 @@ let Hourglass = (size, inc, dec) => {
     }, 1000)
 };
 
-let size = 21 // definir tamanho aqui
+let size = 20 // definir tamanho aqui
 let dec = size - 1
 let inc = 1
 
